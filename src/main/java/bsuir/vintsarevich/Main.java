4d2f28cde2cd7eb48f391cd6bf515f1ab7b671fc
educationGit <31828081+educationGit@users.noreply.github.com>
2018-02-02 23:45:58 +0300

    Test

--- src/main/java/bsuir/vintsarevich/Main.java
+++ src/main/java/bsuir/vintsarevich/Main.java
@@ -1,17 +1,9 @@
 package bsuir.vintsarevich;
 
-        import bsuir.vintsarevich.buisness.order.service.IOrderService;
-        import bsuir.vintsarevich.buisness.orderproduct.service.IOrderProductService;
-        import bsuir.vintsarevich.buisness.product.service.IProductService;
-        import bsuir.vintsarevich.exception.service.ServiceException;
-        import bsuir.vintsarevich.factory.service.ServiceFactory;
-        import org.omg.IOP.IOR;
+import bsuir.vintsarevich.exception.service.ServiceException;
 
 public class Main {
     public static void main(String[] args) throws ServiceException {
-        IProductService productService = ServiceFactory.getInstance().getProducteService();
-        IOrderService orderService=ServiceFactory.getInstance().getOrderService();
-        IOrderProductService orderProductService=ServiceFactory.getInstance().getOrderProductService();
-        orderProductService.deleteOrderProduct(7,9);
+        System.out.println("Hello World");
     }
 }
